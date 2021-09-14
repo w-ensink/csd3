1629a1915d4291c99458ef72bcb3874730ce6be6
Wouter Ensink <wouter@ensink.io>
2021-09-14 23:41:27 +0200

    added basic perspective transformation

---
+++ source/perspective_transformer.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+import unittest
+
+
+def get_top_left(points: [(int, int)]) -> (int, int):
+    points = sorted(points, key=lambda x: x[1])
+    return sorted(points[0:2], key=lambda x: x[0])[0]
+
+
+def get_top_right(points: [(int, int)]) -> (int, int):
+    points = sorted(points, key=lambda x: x[1])
+    return sorted(points[0:2], key=lambda x: x[0])[1]
+
+
+def get_bottom_left(points: [(int, int)]) -> (int, int):
+    points = sorted(points, key=lambda x: x[1])
+    return sorted(points[2:4], key=lambda x: x[0])[0]
+
+
+def get_bottom_right(points: [(int, int)]) -> (int, int):
+    points = sorted(points, key=lambda x: x[1])
+    return sorted(points[2:4], key=lambda x: x[0])[1]
+
+
+def sort_points(points):
+    assert len(points) == 4
+    top_left = get_top_left(points)
+    top_right = get_top_right(points)
+    bottom_left = get_bottom_left(points)
+    bottom_right = get_bottom_right(points)
+    return [top_left, top_right, bottom_left, bottom_right]
+
+
+def user_pressed_esc() -> bool:
+    return cv2.waitKey(24) == 27
+
+
+def get_transform_matrix():
+    with open('transform-matrix.txt', 'r') as f:
+        points = []
+        for _ in range(4):
+            line = f.readline()
+            x, y = line.split(',')
+            points.append([int(x), int(y)])
+        points = sort_points(points)
+        return np.float32(points)
+
+
+def main():
+    cap = cv2.VideoCapture(1)
+    transform_matrix = get_transform_matrix()
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        dst = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+        matrix = cv2.getPerspectiveTransform(transform_matrix, dst)
+        frame = cv2.warpPerspective(frame, matrix, (width, height))
+
+        cv2.imshow('frame', frame)
+
+        if user_pressed_esc():
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# links boven, rechts boven, links onder, rechts onder
+
+class SortTest(unittest.TestCase):
+    def test_sorting(self):
+        points = [[0, 0], [400, 0], [0, 640], [400, 640]]
+        sorted_points = sort_points([[400, 0], [0, 640], [400, 640], [0, 0]])
+        self.assertEqual(sorted_points, points)
+
+    def test_sorting_2(self):
+        points = [[384, 118], [1020, 130], [330, 591], [1027, 640]]
+        sorted_points = sort_points([[384, 118], [1020, 130], [1027, 640], [330, 591]])
+        self.assertEqual(sorted_points, points)
+
+
+if __name__ == '__main__':
+    main()
---
+++ source/setup.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(1)
+draw_positions = []
+COLOR = (255, 0, 0)
+
+
+def user_pressed_esc() -> bool:
+    return cv2.waitKey(24) == 27
+
+
+def mouse_callback(event, x, y, flag, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        draw_positions.append((x, y))
+        print(f'x: {x}, y: {y}')
+
+
+def draw_lines(frame):
+    if len(draw_positions) == 1:
+        frame = cv2.circle(frame, draw_positions[0], 1, COLOR, 2)
+
+    for i in range(len(draw_positions)):
+        if i + 1 < len(draw_positions):
+            frame = cv2.line(frame, draw_positions[i], draw_positions[i + 1], COLOR, 2)
+
+    return frame
+
+
+def write_transform_to_file():
+    with open('transform-matrix.txt', 'w') as f:
+        for x, y in draw_positions:
+            f.writelines(f'{x},{y}\n')
+
+
+def main():
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # Locate points of the documents or object which you want to transform
+        pts1 = np.float32([[0, 260], [640, 260], [0, 400], [640, 400]])
+        pts2 = np.float32([[0, 0], [400, 0], [0, 640], [400, 640]])
+
+        # Apply Perspective Transform Algorithm
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        result = cv2.warpPerspective(frame, matrix, (500, 600))
+        # Wrap the transformed image
+
+        frame = draw_lines(frame)
+
+        cv2.imshow('frame', frame)  # Inital Capture
+        cv2.setMouseCallback('frame', mouse_callback)
+        # cv2.imshow('frame1', result)  # Transformed Capture
+
+        if user_pressed_esc():
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    write_transform_to_file()
+
+
+if __name__ == '__main__':
+    main()
