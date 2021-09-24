500b601d89e60d66e6509f4b9e62467a13ef4508
layetri <daniel@mainstreamct.com>
2021-09-24 13:43:36 +0200

    Adding movement detection and frame freeze function. Extracted draw functions into separate module.

---
+++ source/draw.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+
+# Draw the results on a frame overlay
+def centers(arr, bg, dim, spd, freeze):
+    overlay = Image.new('RGBA', dim, '#00000011')
+    canvas = ImageDraw.Draw(overlay)
+
+    # Draw square at center coordinate
+    for p in arr:
+        canvas.polygon([(p[0] - 5, p[1] - 5), (p[0] - 5, p[1] + 5), (p[0] + 5, p[1] + 5), (p[0] + 5, p[1] - 5)],
+                       'yellow')
+
+    fnt = ImageFont.truetype("../../_assets/arial.ttf", 20)
+    canvas.text((10, 10), str(round(spd, 4)), font=fnt, fill=(255, 255, 255, 255))
+    if freeze:
+        canvas.text((10, 30), "freeze", font=fnt, fill=(255, 255, 255, 255))
+
+    overlay = np.array(overlay)[:, :, ::-1].copy()
+    overlay = cv2.cvtColor(overlay, cv2.COLOR_BGRA2RGBA)
+    return cv2.addWeighted(bg, 0.7, overlay, 0.3, 0)
--- source/object-detection-kmeans.py
+++ source/object-detection-kmeans.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-from PIL import Image, ImageDraw
-import matplotlib.pyplot as plt
+from collections import deque
+import draw
 
 # feature detection
 # - background subtraction
@@ -9,27 +9,36 @@ import matplotlib.pyplot as plt
 # - count black & white pixels to set percentage
 
 # cap = cv2.VideoCapture(2)
+from numpy.random import random
+
 background_subtraction = cv2.createBackgroundSubtractorKNN()
-type = "cam"
-running = True
-w = 480
-h = 320
-
 cap = cv2.VideoCapture(2)
-vid = cv2.VideoCapture('../../_assets/WIN_20210918_10_58_50_Pro.mp4')
+
+running = True
+w = 1366
+h = 768
+freeze_frame = None
+frozen = False
+
+coords_history = deque([])
+accel_history = deque([])
 
 
-def closing(image):
-    kernelSizes = [(3, 3), (5, 5), (7, 7)]
+# Fill histories for rotation + randomize to avoid instant freeze
+def init_history():
+    for _ in range(30):
+        coords_history.append((round(random() * 100), round(random() * 100)))
+    for _ in range(10):
+        accel_history.append(random())
 
-    kn = cv2.getStructuringElement(cv2.MORPH_RECT, kernelSizes[2])
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kn)
+
+init_history()
 
 
+# Prepare incoming frames for analysis
 def preprocess(image):
-    mod = closing(image)
-    c = cv2.cvtColor(mod, cv2.COLOR_RGB2RGBA)
-    Z2 = cv2.findNonZero(mod)
+    c = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
+    Z2 = cv2.findNonZero(image)
 
     if Z2 is not None:
         Z2 = np.squeeze(Z2, axis=1)
@@ -39,74 +48,58 @@ def preprocess(image):
         return c, None
 
 
-def draw_centers(arr, bg):
-    overlay = Image.new('RGBA', (w, h), '#ff000066')
-    canvas = ImageDraw.Draw(overlay)
+# Calculate the average change in location
+def calc_delta_avg():
+    x = 0
+    y = 0
+    for i in range(len(coords_history)):
+        if i > 1 and coords_history[i - 1][0] != 0 and coords_history[i - 1][1] != 0:
+            x += (coords_history[i][0] - coords_history[i - 1][0]) / coords_history[i - 1][0]
+            y += (coords_history[i][1] - coords_history[i - 1][1]) / coords_history[i - 1][1]
+    mean = np.sqrt(pow(x/30, 2) + pow(y/30, 2))
 
-    # Draw square at center coordinate
-    for p in arr:
-        canvas.polygon([(p[0] - 5, p[1] - 5), (p[0] - 5, p[1] + 5), (p[0] + 5, p[1] + 5), (p[0] + 5, p[1] - 5)],
-                       'yellow')
+    accel_history.rotate(1)
+    accel_history[0] = mean
+    return mean
 
-    overlay = np.array(overlay)[:, :, ::-1].copy()
-    return cv2.addWeighted(bg, 0.7, overlay, 0.3, 0)
+
+# Detect movement over time
+def detect_movement():
+    t = 0
+    for i in accel_history:
+        t += i
+    return t / len(accel_history) < 0.004
 
 
 def kMeans(image):
+    global freeze_frame, frozen
     # convert to np.float32
     c, Z2 = preprocess(image)
 
     if Z2 is not None:
-        # define criteria and apply kmeans()
+        # define criteria and apply kMeans()
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
         ret2, label, center = cv2.kmeans(Z2, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-        return draw_centers(center, c)
+        # Calculate average speed per n frames
+        coords_history.rotate(1)
+        coords_history[0] = center[0]
+        speed = calc_delta_avg()
+
+        # Detect movement based on calculated speed
+        freeze = detect_movement()
+        if freeze:
+            freeze_frame = draw.centers(center, c, (w, h), speed, freeze)
+            frozen = True
+            return freeze_frame
+
+        return draw.centers(center, c, (w, h), speed, freeze)
     else:
         return image
 
 
-def prep_video():
-    ret, frame = cap.read()
-
-    return frame
-
-
-def prep_image():
-    im = cv2.imread('../../_assets/test.png')
-    return im
-
-
-wait = True
-while running:
-    if type == 'image':
-        img = cv2.cvtColor(prep_image(), cv2.COLOR_RGB2GRAY)
-    if type == 'video':
-        _, img = vid.read()
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    else:
-        img = prep_video()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    img = cv2.resize(img, (w, h))
-    kernel = np.ones((5, 5), np.uint8)
-
-    foreground_mask = background_subtraction.apply(img)
-    img2 = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)
-
-    # TRYING OUT KMEANS CLUSTERING
-    ol = kMeans(img2)
-    # ============================
-
-    # closing = cv2.add(ol, img)
-    cv2.imshow('frame', img)
-    cv2.imshow('detect', ol)
-
-    while wait:
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            wait = False
-
+# Paul's code
+def do_frame_analysis():
     # moving object
     white_pixels = cv2.countNonZero(img)
     print("white pixels: ", white_pixels)
@@ -118,9 +111,37 @@ while running:
     percentage_white_pixels = (white_pixels / black_pixels) * 100
     print("percentage of white pixels: ", percentage_white_pixels, "\n")
 
+
+# Main program loop
+while running:
+    # Get and resize
+    ret, frame = cap.read()
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (w, h))
+
+    # Run masking process
+    kernel = np.ones((5, 5), np.uint8)
+    foreground_mask = background_subtraction.apply(img)
+    img2 = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)
+
+    # Perform image processing
+    if not frozen:
+        ol = kMeans(img2)
+        do_frame_analysis()
+    else:
+        ol = freeze_frame
+    # ============================
+
+    # Display the window
+    cv2.imshow('detect', ol)
+
+    # Handle keyboard input
     k = cv2.waitKey(30) & 0xff
     if type == "image" or k == 27:
         running = False
+    elif k == 32:
+        frozen = False
+        init_history()
 
 cap.release()
 cv2.destroyAllWindows()
