63637a226b3cb5d264b2e666efb456773566c0db
N1N036 <70594048+N1N036@users.noreply.github.com>
2021-10-11 11:41:24 +0200

    setup gecommit

--- source/touch_designer/setup.py
+++ source/touch_designer/setup.py
@@ -2,13 +2,13 @@
 import sys
 
 users = {
-    "nino": ['', '', ''],
+    "nino": ['C:\\Users\\ninos\\Dropbox\\github\\csd3_whisperingshadows\\source', 'C:\\Users\\ninos\\anaconda3\\lib', 'C:\\Users\\ninos\\anaconda3\\lib\\site-packages'],
     "daniel": ['/Users/layetri/Development/csd3/source', '/Users/layetri/opt/anaconda3/lib/python3.8', '/Users/layetri/opt/anaconda3/lib/python3.8/site-packages'],
     "wouter": ['/Users/wouter/Documents/School/Jaar_3/CSD/csd3/source', '/Users/wouter/anaconda3/envs/source/lib/python3.7', '/Users/wouter/anaconda3/envs/source/lib/python3.7/site-packages'],
     "paul": ['', '', ''],
 }
 
-user = "daniel"
+user = "nino"
 
 sys.path.append(users[user][0])
 sys.path.append(users[user][1])
