4a45e76024703374da84538825af87a01f2218d7
layetri <daniel@mainstreamct.com>
2021-10-21 19:28:08 +0200

    Added source urls. Optimized and cleaned up kMeans code.

--- source/object_detection_kmeans.py
+++ source/object_detection_kmeans.py
@@ -3,23 +3,9 @@ import cv2
 from collections import deque
 from numpy.random import random
 
-from scipy.cluster.vq import kmeans
-
-if __name__ == "__main__":
-    import draw
-
-background_subtraction = cv2.createBackgroundSubtractorKNN()
-
 NUMBER_OF_CLUSTERS = 4
 DISTANCE_THRESHOLD = 20
-old_centers = []
 old_mean = (0, 0)
-
-running = True
-w = 1366
-h = 768
-freeze_frame = None
-frozen = False
 
 coords_history = deque([])
 accel_history = deque([])
@@ -37,30 +23,29 @@ init_history()
 
 
 # Prepare incoming frames for analysis
-def preprocess(image, is_arr=False):
-    c = None
-    if not is_arr:
-        c = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
-        image = (255 - image)
-        image = cv2.findNonZero(image)
+
+def preprocess(image):
+    image = cv2.findNonZero(255 - image)
 
     if image is not None:
+        # Taken from https://docs.opencv.org/master/d1/d5c/tutorial_py_kmeans_opencv.html (Step 3)
         Z2 = np.squeeze(image, axis=1)
         Z2 = Z2.reshape((-1, 2))
-        return c, np.float32(Z2)
+        return np.float32(Z2)
     else:
-        return c, None
+        return None
 
 
 # Calculate the average change in location
 def calc_delta_avg():
-    x = 0
-    y = 0
+    x, y = 0, 0
+
     # Sum coordinates history
     for i in range(len(coords_history)):
         if i > 1 and coords_history[i - 1][0] != 0 and coords_history[i - 1][1] != 0:
             x += (coords_history[i][0] - coords_history[i - 1][0]) / coords_history[i - 1][0]
             y += (coords_history[i][1] - coords_history[i - 1][1]) / coords_history[i - 1][1]
+
     # Calculate Euclidean distance for acceleration
     mean = np.sqrt(pow(x/30, 2) + pow(y/30, 2))
 
@@ -74,6 +59,7 @@ def detect_movement():
     t = 0
     for i in accel_history:
         t += i
+    
     # Calculate the average acceleration and compare to threshold
     return t / len(accel_history) < 0.004
 
@@ -81,12 +67,13 @@ def detect_movement():
 # Perform the main image processing and analysis tasks
 def process_frame(image):
     # Do data preprocessing
-    c, Z2 = preprocess(image)
+    Z2 = preprocess(image)
 
     if Z2 is not None:
         # Perform the kMeans cluster analysis
+        # Using code from https://www.thepythoncode.com/article/kmeans-for-image-segmentation-opencv-python
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        ret2, label, center = cv2.kmeans(Z2, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        _, _, center = cv2.kmeans(Z2, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
         # Calculate average speed over frames
         coords_history.rotate(1)
@@ -95,64 +82,3 @@ def process_frame(image):
         return center
     else:
         return []
-
-
-def process_contours(contours):
-    global old_mean
-
-    rtn, arr = [], []
-    arranged = sorted(contours, key=cv2.contourArea, reverse=True)
-
-    total_x = 0
-    total_y = 0
-
-    for o in arranged[:NUMBER_OF_CLUSTERS]:
-        _, o = preprocess(o, True)
-        o = kmeans(o, 1)[0].flatten()
-        rtn.append(o)
-        total_x += o[0]
-        total_y += o[1]
-
-    # Apply smoothing
-    mean = (total_x / (len(rtn) + 0.0001), total_y / (len(rtn) + 0.0001))
-    avg_mean = ((mean[0] + old_mean[0]) / 2., (mean[1] + old_mean[1]) / 2.)
-    old_mean = avg_mean
-
-    return avg_mean
-
-
-if __name__ == "__main__":
-    cap = cv2.VideoCapture(2)
-
-    # Main program loop
-    while running:
-        # Get and resize
-        ret, frame = cap.read()
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (w, h))
-
-        # Run masking process
-        kernel = np.ones((5, 5), np.uint8)
-        foreground_mask = background_subtraction.apply(img)
-        img2 = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)
-
-        # Perform image processing
-        if not frozen:
-            ol = process_frame(img2, True)
-        else:
-            ol = freeze_frame
-        # ============================
-
-        # Display the window
-        cv2.imshow('detect', ol)
-
-        # Handle keyboard input
-        k = cv2.waitKey(30) & 0xff
-        if type == "image" or k == 27:
-            running = False
-        elif k == 32:
-            frozen = False
-            init_history()
-
-    cap.release()
-    cv2.destroyAllWindows()
